558ae21fd23a4a56e467f9d0f8235385a215347e
Julian Voros <julianvoros@hotmail.com>
2023-10-25 17:40:55 -0600

    Added cameraController script

---
+++ Whispers of the Warlock/Assets/Scripts/cameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] int sensitivity;    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
