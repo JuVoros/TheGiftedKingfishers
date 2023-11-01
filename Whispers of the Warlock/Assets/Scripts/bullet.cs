4d552b6843266d6926e4a4c05025fa1c0c55e134
Julian Voros <julianvoros@hotmail.com>
2023-11-01 12:19:18 -0600

    Started bullet Script

---
+++ Whispers of the Warlock/Assets/Scripts/bullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bul : MonoBehaviour
+{
+    [Header("---- Components ----")]
+    [SerializeField] Rigidbody rb;
+
+    [Header("---- Stats ----")]
+    [SerializeField] int damage;
+    [SerializeField] int speed;
+    [SerializeField] int destroyTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
